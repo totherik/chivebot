e2a435868efba6745547b9b0bd946266cb9954e5
ertoth <ertoth@paypal.com>
2014-02-05 21:16:20 -0800

    note

--- example/index.js
+++ example/index.js
@@ -4,6 +4,8 @@ var Hapi = require('hapi'),
     settings = require('./config'),
     server = settings.servers[0];
 
+// NOTE: This all could be done using hapi composer config files.
+// and by running `hapi -c config.json`
 
 var app = new Hapi.Server(server.host, server.port);
 app.pack.require('../', settings.plugins.chivebot, function (err) {
